5e9fb07c6983d07c4ae728dbcbb558a4fd30ae61
Philip Loa <boyet007@gmail.com>
2018-12-09 23:21:10 +0700

    #4 The query selector

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var titles = document.getElementsByClassName('title')
+const wrap = document.querySelector('#wrapper')
 
-//console.log(Array.isArray(titles)) --> false
-//console.log(Array.isArray(Array.from(titles))) --> true
+//cari query class di li yang kedua 
+const wrap2 = document.querySelector('#book-list li:nth-child(2) .name')
 
-// titles.array.forEach(item => {
-//     console.log(item)
-// });
+//hanya dokumenyang pertama ditampilkan
+var books = document.querySelector('#book-list li .name')
 
-Array.from(titles).forEach(function(item){
-    console.log(item)
+//dokumen semua ditampilkan
+books = document.querySelectorAll('#book-list li .name')
+
+//tampilkan setiap bagian dari array
+Array.from(books).forEach(function(book) {
+    console.log(book)
 })
+console.log(books)
