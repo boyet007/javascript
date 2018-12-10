9a1aa56574b4b34070b39c110c365dc7a724b06b
Philip Loa <boyet007@gmail.com>
2018-12-10 09:37:54 +0700

    #6 Nodes

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-var books = document.querySelectorAll('#book-list li .name')
+const banner = document.querySelector('#page-banner')
 
-console.log(books)
+console.log('#page-banner node type is: ' + banner.nodeType)
+console.log('#page-banner node type is: ' + banner.nodeName)
+console.log('#page-banner has child is: ' + banner.hasChildNodes())
 
-Array.from(books).forEach(function(book) {
-    //console.log(book.textContent)
-
-    //tambahkan setiap judul dengan (booktitle)
-    book.textContent += ' (book title)'
-})
-
-const bookList = document.querySelector('#book-list')
-
-//overwrite semua element dengan text
-bookList.innerHTML = '<h2>Books and More Books ... </h2>'
-
-//tambahkan elemen di dalam elemen
-bookList.innerHTML += '<p>This how you add HTML</p>'
-
-//tampilan pure html
-console.log(bookList.innerHTML)
-
+//kopi seluruh isi node didalam page-banner kedalam clonebanner
+const clonedBanner = banner.cloneNode(true)
+console.log(clonedBanner)
