f12c2beb190d99db31d947de1787d455156367ed
Philip Loa <boyet007@gmail.com>
2018-12-10 16:13:36 +0700

    #15 CheckBox and Change Events

--- app.js
+++ app.js
@@ -32,11 +32,26 @@ addForm.addEventListener('submit', function(e) {
     li.appendChild(bookName)
     li.appendChild(deleteBtn)
     list.appendChild(li)
+
     
-
-
-
 })
+
+//hide books
+const hideBox = document.querySelector('#hide')
+hideBox.addEventListener('change', function(e) {
+    console.log(e)
+    if(hideBox.checked) {
+        list.style.display = 'none'
+    } else {
+        list.style.display = 'initial'
+    }
+})
+
+
+      
+
+
+
 
 
 
